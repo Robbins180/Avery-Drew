372166d7d576c826a82c884e8d90ed956c7d11a8
Avery Sarichny <averysarichny@cpe-67-240-191-189.rochester.res.rr.com>
2021-01-13 17:26:58 -0500

    added env file and mongoose to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 //===============
 const express = require('express')
 const app = express()
-const PORT = 3000;
+const mongoose = require('mongoose')
 
 
 //=================
@@ -13,36 +13,36 @@ app.use(express.json())
 app.use(express.static('public'))
 
 
-//==================
+// ==================
 //  CONFIG
-//==================
-// require('dotenv').config()
-// const PORT = process.env.PORT
-// const MONGODB_URI = process.env.MONGODB_URI
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false
-// })
+// ==================
+require('dotenv').config()
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
 
-//===============
+// ===============
 //  ERRORS + YES
-//===============
+// ===============
 
-// mongoose.connection.on('error', err =>
-//   console.log(
-//     err.message,
-//     ' is Mongod not running?/Problem with Atlas Connection?'
-//   )
-// )
-// mongoose.connection.on('connected', () =>
-//   console.log('mongo connected: ', MONGODB_URI)
-// )
-// mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+mongoose.connection.on('error', err =>
+  console.log(
+    err.message,
+    ' is Mongod not running?/Problem with Atlas Connection?'
+  )
+)
+mongoose.connection.on('connected', () =>
+  console.log('mongo connected: ', MONGODB_URI)
+)
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
-//=================
+// =================
 //  CONTROLLER
-//=================
+// =================
 
 app.get('/alice', (req, res) => {
   res.send("hello world")
