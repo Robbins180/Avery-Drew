0e984c34bf8881e10eda8ca0909a890a0645d9bf
Avery Sarichny <averysarichny@cpe-67-240-191-189.rochester.res.rr.com>
2021-01-13 20:16:11 -0500

    create form and index populate to page, added handle and submit requests as well

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,60 @@
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    name:"",
-    img:"",
+    name: "",
+    image: "",
+    characters: [],
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value })
   }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    axios
+    .post('/alice', this.state)
+    .then(response =>
+    this.setState({ char: response.data, name: '', image: '' })
+    )
+  }
+
+
+  componentDidMount = () => {
+    axios.get("/alice").then((response) => {
+      this.setState({
+        characters: response.data,
+      });
+    });
+  };
 
   render = () => {
     return (
       <div>
         <h1>Alice In Wonderland</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <br />
+          <input type="text" id="name" onChange={this.handleChange} />
+          <br />
+          <label htmlFor="image">Image</label>
+          <br />
+          <input type="text" id="image" onChange={this.handleChange} />
+          <br />
+          <input type="submit" value="Add Character" />
+        </form>
+        <h2>Characters</h2>
+          <ul>
+            {this.state.characters.map((char) => {
+              return (
+                <li key={char._id}>{char.name} <br />
+                <img src={char.image} alt={char.name} />
+                </li>
+              )
+            })}
+          </ul>
       </div>
-    )
-  }
+    );
+  };
 }
 
-ReactDOM.render(
-  <App></App>,
-  document.querySelector('main')
-)
+ReactDOM.render(<App></App>, document.querySelector("main"));
