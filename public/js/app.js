613dc7bc1557ec91859a1282f850eb6967a0cefa
Drew R <drewr@Drews-MacBook-Pro.local>
2021-01-13 18:23:02 -0600

    app

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-console.log('alice');
+// console.log('alice');
+
+class App extends React.Component {
+
+  render = () => {
+    return (
+      <div>
+        <h2>Create a Character</h2>
+      </div>
+    )
+  }
+
+}
+
+ReactDOM.render(<App></App>, document.querySelector('main'))
