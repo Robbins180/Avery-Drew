7065f8e2dccdea5686e24ae2a0b7803b9bb4fab3
Avery Sarichny <averysarichny@cpe-67-240-191-189.rochester.res.rr.com>
2021-01-13 23:01:02 -0500

    added create route

--- public/js/app.js
+++ public/js/app.js
@@ -6,15 +6,21 @@ class App extends React.Component {
   };
 
   handleChange = event => {
-    this.setState({ [event.target.id]: event.target.value })
-  }
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
 
   handleSubmit = event => {
     event.preventDefault();
     axios
     .post('/alice', this.state)
     .then(response =>
-    this.setState({ char: response.data, name: '', image: '' })
+    this.setState({
+      characters: response.data,
+      name:'',
+      image:'',
+      })
     )
   }
 
@@ -35,28 +41,52 @@ class App extends React.Component {
     })
   }
 
-    updateAlice = event => {
-    event.preventDefault()
-    const id = event.target.id
-    axios.put('/alice/' + id, this.state).then(response => {
-      this.setState({
-        characters: response.data,
-        name: '',
-        image: ''
-        })
+  updateAlice = event => {
+  event.preventDefault()
+  const id = event.target.id
+  axios.put('/alice/' + id, this.state).then(response => {
+    this.setState({
+      characters: response.data,
+      name: '',
+      image: ''
       })
-    }
+    })
+  }
 
     render = () => {
         return (
           <div>
             <h2>Create Character</h2>
-
-              <h2>List of Characters made</h2>
+            <details>
+            <summary>Add An Character</summary>
+              <form
+                onSubmit={this.handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input
+                type="text"
+                id="name"
+                onChange={this.handleChange}
+                value={this.state.name}
+                />
+                <br />
+                <label htmlFor="image">Image</label>
+                <input
+                type="text"
+                id="image"
+                onChange={this.handleChange}
+                value={this.state.image}
+                />
+                <br />
+              <input
+              type="submit"
+              value="Add Character" />
+              </form>
+              </details>
+              <h2>List of Characters</h2>
                 <ul>
                 {this.state.characters.map((alice) => {
                     return (
-                      <li>
+                      <li key={alice._id}>
                       {alice.name} <br />
                       <img src={alice.image} alt={alice.name} />
                       <details>
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const cors = require('cors')
+
 
 
 //=================
@@ -12,7 +12,7 @@ const cors = require('cors')
 //=================
 app.use(express.json())
 app.use(express.static('public'))
-app.use(cors({origin: '*'}));
+
 
 
 // ==================
